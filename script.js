a735ce6eb827e8d30af2f48cf7a222f559ee2fba
Biswajit <biswajitprusty165@gmail.com>
2021-03-25 23:03:37 +0530

    Every thing is updated

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-function submit(){
+function submit() {
     var option = document.getElementById("box").value;
     // var type = document.getElementById("addLink").getAttribute("href");
-    if(option ==1  ){
-        document.getElementById("addLink").setAttribute("href","addBookDetails.html");
-        document.getElementById("addLink").setAttribute("target","black");
+    if (option == 1) {
+        document.getElementById("addLink").setAttribute("href", "addBookDetails.html");
+        document.getElementById("addLink").setAttribute("target", "black");
     }
-    else if(option ==2 ){
-        document.getElementById("addLink").setAttribute("href","searchBook.html");
-        document.getElementById("addLink").setAttribute("target","black");
+    else if (option == 2) {
+        document.getElementById("addLink").setAttribute("href", "searchBook.html");
+        document.getElementById("addLink").setAttribute("target", "black");
 
-
-    }
-    else if(option ==3 ){
-        document.getElementById("addLink").setAttribute("href","updatebook.html");
-        document.getElementById("addLink").setAttribute("target","black");
 
     }
-    else if(option ==4 ){
-        document.getElementById("addLink").setAttribute("href","removeBook.html");
-        document.getElementById("addLink").setAttribute("target","black");
+    else if (option == 3) {
+        document.getElementById("addLink").setAttribute("href", "updatebook.html");
+        document.getElementById("addLink").setAttribute("target", "black");
 
     }
-    else{
+    else if (option == 4) {
+        document.getElementById("addLink").setAttribute("href", "removeBook.html");
+        document.getElementById("addLink").setAttribute("target", "black");
 
-        document.getElementById("msg").innerHTML="Please Select From Above Options".fontcolor("red");
+    }
+    else {
+
+        document.getElementById("msg").innerHTML = "Please Select From Above Options".fontcolor("red");
         document.getElementById("addLink").removeAttribute("href");
     }
 }
